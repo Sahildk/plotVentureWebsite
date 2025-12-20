7acf7fbc334d4d86f3ff49f81e117102b7ac5357
Sahil D <114382046+Sahildk@users.noreply.github.com>
2025-12-20 22:51:54 +0530

    backend update

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -7,11 +7,6 @@ export default ({ env }) => ({
         api_key: env('CLOUDINARY_KEY'),
         api_secret: env('CLOUDINARY_SECRET'),
       },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
-      },
     },
   },
 });
